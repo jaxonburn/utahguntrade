84c496ac49cae41c799de0c8d12d83abde7a9263
jaxonburningham <jaxon@ionrev.com>
2021-02-27 19:36:35 -0700

    Added articles page to main side.

---
+++ client/src/components/common/SocialLinks.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+  <q-list v-for="(link,index) in links" :key="index">
+    <a :href="'//' + link.link" target="_blank">
+      <q-item class="linkItems">
+        <q-item-section avatar>
+          <q-avatar>
+            <q-img :src="link.picture" v-if="link.picture !== ''"/>
+            <q-icon :name="link.icon" v-if="link.icon !== ''" size="md" :style="{color: link.color}"></q-icon>
+          </q-avatar>
+        </q-item-section>
+        <q-item-section class="aboutFont" style="font-size: 1.8em;text-align: left;">
+          {{ link.name }}
+        </q-item-section>
+      </q-item>
+    </a>
+    <q-separator/>
+  </q-list>
+  </div>
+</template>
+
+<script>
+import instagramLogo from 'assets/Logos/instagramLogo.jpg';
+
+export default {
+name: 'SocialLinks',
+  data() {
+    return {
+      links: [
+        {
+          name: 'Facebook',
+          picture: '',
+          icon: 'fab fa-facebook-square',
+          color: '#4C6EF5',
+          link: 'www.facebook.com',
+        },
+        {
+          name: 'Instagram',
+          picture: instagramLogo,
+          icon: '',
+          color: '',
+          link: 'www.instagram.com'
+        },
+        {
+          name: 'Twitter',
+          picture: '',
+          icon: 'fab fa-twitter',
+          color: '#05A1EE',
+          link: 'www.twitter.com'
+        },
+        {
+          name: 'Youtube',
+          picture: '',
+          icon: 'fab fa-youtube',
+          color: '#FF0102',
+          link: 'www.youtube.com'
+        }
+      ]
+    }
+  },
+};
+</script>
+
+<style scoped>
+
+  .linkItems {
+    transition: all .3s ease-in-out;
+    cursor: pointer;
+    -webkit-box-shadow: inset 10px 0 3px -3px #D4212E, 0px 0px 0px 5px rgba(0, 0, 0, 0);
+    box-shadow: inset 10px 0 3px -3px #D4212E, 0px 0px 0px 5px rgba(0, 0, 0, 0);
+  }
+
+  .linkItems:hover {
+    transform: scale(1.03);
+    background: #cfd0d1;
+  }
+</style>
---
+++ client/src/pages/BrowseArticles.vue
@@ -0,0 +1,131 @@
+<template>
+  <q-page>
+    <div style="width: 100%;display: flex;flex-direction: row;">
+      <div
+        style="width: 60%;height: 500px;border-right: 0.5px solid #bfbfbf;display: flex; flex-direction: column;justify-content: space-evenly;">
+        <div
+          :style="{backgroundImage: `url(${$lget(articles, '[0].mainImage.url', '')})`,}"
+          class="backGroundImage"
+        >
+          <div class="q-mt-lg text-black text-center readFont"
+               style="width: 40%;height: 30px; border-top-right-radius: 15px;border-bottom-right-radius: 15px;background: rgba(215, 215, 215, 0.8)">
+            Newest Article
+          </div>
+        </div>
+        <div>
+
+        </div>
+        <div class="readFont">
+          {{ $lget(articles, '[0].mainTitle', 'Gun Hub') }}
+        </div>
+        <div class="aboutFont q-mb-md">
+          <span>{{ $lget(articles, '[0].createdBy', 'Anonymous') }}</span>
+          <q-icon name="fiber_manual_record" style="font-size: 0.6em;" class="q-mx-sm text-accent"></q-icon>
+          <span>{{ $lget(articles, '[0].readTime', '10') + ' Minute Read' }}</span>
+        </div>
+        <div>
+          <div style="height: 60px;text-overflow: ellipsis;overflow: hidden;position: relative;font-size: 1.3em;"
+               class="q-mx-xl readFont">
+            <div v-html="articles[0].sections[0].body"></div>
+          </div>
+          <div class="flex justify-end">
+            <q-btn class="q-mr-lg q-mb-sm bg-accent text-white" push
+                   @click="$router.push({name: 'article', params: {id:  articles[0]._id}})">Read More
+            </q-btn>
+          </div>
+        </div>
+        <div class="flex justify-between avatarFont q-mx-md q-mb-sm">
+          <div class="flex flex-center">
+            <q-icon style="font-size: 1.5em;" name="fas fa-heart" color="red"></q-icon>
+            <span style="font-size: 1.5em;">{{ $lget(articles, '[0].favorites.length', 0) }}</span>
+            <q-icon style="font-size: 1.6em;" name="fas fa-comments" color="light-blue" class="q-ml-md"/>
+            <span class="q-ml-xs" style="font-size: 1.5em;">{{ $lget(articles, '[0].comments.length', 0) }}</span>
+          </div>
+          <div>
+            Published {{ $lget(articles, '[0].publishedAt', '')| format }} ago
+          </div>
+        </div>
+      </div>
+      <div style="display: flex; justify-content: center;align-items: center;width: 40%;" class="q-ma-md">
+        <q-card style="width: 100%; height: 50%;display: flex;justify-content: center;flex-direction: column;" flat>
+          <q-card-section class="aboutFont" style="font-size: 2em;display: flex; justify-content: center;border-bottom: 1px solid var(--q-color-primary);">
+            See More From Utah Gun Hub
+          </q-card-section>
+          <q-card-section>
+            <social-links>
+
+            </social-links>
+          </q-card-section>
+        </q-card>
+      </div>
+    </div>
+    <q-separator/>
+  </q-page>
+</template>
+
+<script>
+  import {makeFindMixin} from 'feathers-vuex';
+  import dateUtils from 'src/helpers/dateUtils';
+  import instagramLogo from '../assets/Logos/instagramLogo.jpg';
+  import SocialLinks from 'components/common/SocialLinks';
+
+  export default {
+    name: 'BrowseArticles',
+    components: {SocialLinks},
+    filters: {
+      format(date) {
+        let newDate = new Date(date);
+        return dateUtils(newDate);
+      }
+    },
+    mixins: [
+      makeFindMixin({
+        name: 'articles',
+        service: 'articles',
+        qid: 'articles'
+      })
+    ],
+    computed: {
+      articlesParams() {
+        return {query: {published: true}}
+      }
+    }
+  };
+</script>
+
+<style scoped>
+  .backGroundImage {
+    height: 70%;
+    width: 100%;
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: contain;
+  }
+
+  .readFont {
+    font-family: "Kuchek", Times, Serif;
+    line-height: 1.6em;
+    font-size: 1.3em;
+    font-weight: 100;
+    text-align: center;
+  }
+
+  .avatarFont {
+    font-family: "KuchekMedium", Times, Serif;
+    line-height: 1.6em;
+    font-weight: 350;
+    font-size: 1em;
+  }
+
+
+  .aboutFont {
+    font-family: "KuchekThin", Times, Serif;
+    line-height: 1em;
+    font-weight: 600;
+    font-size: 1em;
+    text-align: center;
+  }
+
+
+
+</style>
